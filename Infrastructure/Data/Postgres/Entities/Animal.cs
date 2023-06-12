f51c241bce1c9a2f17b8e90103ae99706346d904
Elif ÇÖL <elif.col@std.yeditepe.edu.tr>
2023-06-12 19:54:29 +0300

    entity deneme...

--- Infrastructure/Data/Postgres/Entities/Animal.cs
+++ Infrastructure/Data/Postgres/Entities/Animal.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure.Data.Postgres.Entities.Base;
+﻿using System;
+using Infrastructure.Data.Postgres.Entities.Base;
+using Infrastructure.Data.Postgres.Entities.Infrastructure.Data.Postgres.Entities;
 
 namespace Infrastructure.Data.Postgres.Entities;
 
@@ -7,16 +9,11 @@ public class Animal : Entity<int>
 
     public string animal_name { get; set; } = default!;
     public string animal_year { get; set; } = default!;
-    public Boolean animal_vaccine { get; set; };
-    public Boolean animal_chip { get; set; };
+    public bool animal_vaccine { get; set; };
+    public bool animal_chip { get; set; };
     public string animal_sex { get; set; } = default!;
     public string animal_type { get; set; } = default!;
-    public Boolean animal_passion { get; set; };
-
-   // deneme 
-
-
-
+    public bool animal_passion { get; set; };
 
 }
 
--- Infrastructure/Data/Postgres/Entities/User.cs
+++ Infrastructure/Data/Postgres/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Data.Postgres.Entities.Base;
+﻿using System;
+using Infrastructure.Data.Postgres.Entities.Base;
 using Infrastructure.Data.Postgres.Entities.Infrastructure.Data.Postgres.Entities;
 
 namespace Infrastructure.Data.Postgres.Entities;
@@ -8,14 +9,13 @@ public class User : Entity<int>
     
     public string user_name { get; set; } = default!;
     public string user_surname { get; set; } = default!;
-    public DateTime user_bdate { get; set; } = default!;
+    public string user_bdate { get; set; } = default!;
     public string user_adress { get; set; } = default!;
     public char[] user_sex { get; set; } = default!;
     public string user_mail { get; set; } = default!;
     public string user_gsm { get; set; } = default!;
-    public Boolean animal_history { get; set; };
-    public Boolean animal_exist { get; set; };
-
+    public bool animal_history { get; set; };
+    public bool animal_exist { get; set; };
     public int animal_id { get; set; } = default!;
     public int advert_id { get; set; } = default!;
     public int chat_id { get; set; } = default!;
@@ -23,12 +23,12 @@ public class User : Entity<int>
     public byte[] PasswordSalt { get; set; } = default!;
     public byte[] PasswordHash { get; set; } = default!;
     public UserType UserType { get; set; }
-    public Animal Animal { get; set; };
+    public Animal Animal { get; set; }
   
 
     
 }
-//yorum satırı 
+
 public enum UserType
 {
     Admin,
