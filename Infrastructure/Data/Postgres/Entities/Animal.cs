2aaf6d3226f5591c99ec6a38fdf01ee19694d3b0
Elif ÇÖL <elif.col@std.yeditepe.edu.tr>
2023-06-12 19:22:10 +0300

    yorum satırı girdim...

---
+++ Infrastructure/Data/Postgres/Entities/Animal.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Data.Postgres.Entities.Base;
+
+namespace Infrastructure.Data.Postgres.Entities;
+
+public class Animal : Entity<int>
+{
+
+    public string animal_name { get; set; } = default!;
+    public string animal_year { get; set; } = default!;
+    public Boolean animal_vaccine { get; set; };
+    public Boolean animal_chip { get; set; };
+    public string animal_sex { get; set; } = default!;
+    public string animal_type { get; set; } = default!;
+    public Boolean animal_passion { get; set; };
+
+
+
+   
+
+}
+
+
--- Infrastructure/Data/Postgres/Entities/User.cs
+++ Infrastructure/Data/Postgres/Entities/User.cs
@@ -4,14 +4,30 @@ namespace Infrastructure.Data.Postgres.Entities;
 
 public class User : Entity<int>
 {
-    public string Email { get; set; } = default!;
-    public string UserName { get; set; } = default!;
-    public string FullName { get; set; } = default!;
+    
+    public string user_name { get; set; } = default!;
+    public string user_surname { get; set; } = default!;
+    public DateTime user_bdate { get; set; } = default!;
+    public string user_adress { get; set; } = default!;
+    public char[] user_sex { get; set; } = default!;
+    public string user_mail { get; set; } = default!;
+    public string user_gsm { get; set; } = default!;
+    public Boolean animal_history { get; set; };
+    public Boolean animal_exist { get; set; };
+
+    public int animal_id { get; set; } = default!;
+    public int advert_id { get; set; } = default!;
+    public int chat_id { get; set; } = default!;
+
     public byte[] PasswordSalt { get; set; } = default!;
     public byte[] PasswordHash { get; set; } = default!;
     public UserType UserType { get; set; }
-}
+    public Animal Animal { get; set; };
+  
 
+    
+}
+//yorum satırı
 public enum UserType
 {
     Admin,
