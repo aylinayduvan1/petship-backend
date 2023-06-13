20498d7921e8ede210d15f61d2f407105a9bda4f
aylinayduvan <aylinnayduvann@gmail.com>
2023-06-13 11:02:54 +0300

    Entities refactor

--- Infrastructure/Data/Postgres/Entities/Animal.cs
+++ Infrastructure/Data/Postgres/Entities/Animal.cs
@@ -9,11 +9,11 @@ public class Animal : Entity<int>
 
     public string animal_name { get; set; } = default!;
     public string animal_year { get; set; } = default!;
-    public bool animal_vaccine { get; set; };
-    public bool animal_chip { get; set; };
+    public bool animal_vaccine { get; set; }
+    public bool animal_chip { get; set; }
     public string animal_sex { get; set; } = default!;
     public string animal_type { get; set; } = default!;
-    public bool animal_passion { get; set; };
+    public bool animal_passion { get; set; }
 
 }
 
--- Infrastructure/Data/Postgres/Entities/User.cs
+++ Infrastructure/Data/Postgres/Entities/User.cs
@@ -14,18 +14,15 @@ public class User : Entity<int>
     public char[] user_sex { get; set; } = default!;
     public string user_mail { get; set; } = default!;
     public string user_gsm { get; set; } = default!;
-    public bool animal_history { get; set; };
-    public bool animal_exist { get; set; };
+    public bool animal_history { get; set; }
+    public bool animal_exist { get; set; }
     public int animal_id { get; set; } = default!;
     public int advert_id { get; set; } = default!;
-    public int chat_id { get; set; } = default!;
-
     public byte[] PasswordSalt { get; set; } = default!;
     public byte[] PasswordHash { get; set; } = default!;
     public UserType UserType { get; set; }
     public Animal? Animal { get; set; }
     public Advert? Advert { get; set; }
-    public Message? Message { get; set; }
 }
 
 public enum UserType
