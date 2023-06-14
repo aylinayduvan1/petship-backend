b69d1115f678660b41e581cdfbc4713bee02473d
muhammedsafatur <msafatur@outlook.com>
2023-06-14 11:34:35 +0300

    Advert Configuration Added

--- Infrastructure/Data/Postgres/EntityFramework/Configurations/AdvertConfiguration.cs
+++ Infrastructure/Data/Postgres/EntityFramework/Configurations/AdvertConfiguration.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Data.Postgres.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Postgres.EntityFramework.Configurations
 {
-    internal class AdvertConfiguration
+    public class AdvertConfiguration : IEntityTypeConfiguration<Advert>
     {
+        public void Configure(EntityTypeBuilder<Advert> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.advert_no).IsRequired();
+            builder.HasIndex(x => x.advert_no).IsUnique();
+            builder.Property(x => x.advert_date).IsRequired();
+            builder.Property(x => x.advert_title).IsRequired();
+            builder.Property(x => x.advert_text).IsRequired();
+        }
     }
 }
+ 
