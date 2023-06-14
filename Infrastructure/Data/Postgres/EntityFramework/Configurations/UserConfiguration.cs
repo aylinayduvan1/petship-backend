212cae16cf4fa2eb8e88e9681c2a8abe36aa6b7f
caglaozy <caglaozyy@gmail.com>
2023-06-14 10:29:25 +0300

    User Config created

--- Infrastructure/Data/Postgres/EntityFramework/Configurations/UserConfiguration.cs
+++ Infrastructure/Data/Postgres/EntityFramework/Configurations/UserConfiguration.cs
@@ -9,14 +9,32 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.HasKey(x => x.Id);
+        builder.Property(x => x.UserName).IsRequired();
+        builder.Property(x => x.Password).IsRequired();
+        builder.Property(x => x.user_surname).IsRequired();
+        builder.HasIndex(x => x.user_surname).IsUnique();
+        builder.Property(x => x.user_adress).IsRequired();
+        builder.Property(x => x.user_sex).IsRequired().HasMaxLength(1);
         builder.Property(x => x.Email).IsRequired();
         builder.HasIndex(x => x.Email).IsUnique();
-        builder.Property(x => x.UserName).IsRequired();
-        builder.HasIndex(x => x.UserName).IsUnique();
-        //builder.Property(x => x.FullName).IsRequired();
-        builder.Property(x => x.PasswordHash).IsRequired();
+        builder.Property(x => x.user_gsm).IsRequired();
+        builder.Property(x => x.animal_id).IsRequired();
+        builder.Property(x => x.advert_id).IsRequired();
         builder.Property(x => x.PasswordSalt).IsRequired();
+        builder.Property(x => x.PasswordHash).IsRequired();
         builder.Property(x => x.CreatedAt).IsRequired();
         builder.Property(x => x.IsDeleted).IsRequired();
+
+        builder.HasOne(x => x.Animal)
+            .WithOne()
+            .HasForeignKey<User>(x => x.animal_id);
+
+        builder.HasOne(x => x.Advert)
+            .WithOne()
+            .HasForeignKey<User>(x => x.advert_id);
+
+        builder.Property(x => x.UserType)
+            .IsRequired()
+            .HasConversion<string>();
     }
 }
