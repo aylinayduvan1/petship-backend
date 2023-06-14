bbaf5b54a1625b74b8dadd4cd6edb882407ec4d7
Muhammed Safa Tur <44067638+muhammedsafatur@users.noreply.github.com>
2023-06-14 11:36:38 +0300

    AdvertConfiguration.cs

--- Infrastructure/Data/Postgres/EntityFramework/Configurations/AdvertConfiguration.cs
+++ Infrastructure/Data/Postgres/EntityFramework/Configurations/AdvertConfiguration.cs
@@ -15,7 +15,7 @@ namespace Infrastructure.Data.Postgres.EntityFramework.Configurations
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.advert_no).IsRequired();
-            builder.HasIndex(x => x.advert_no).IsUnique();
+            builder.HasIndex(x => x.advert_no).IsUnique(); // Burada Advert No'yu Unique olduğunu belirttik bu sayede karışıklık ihtimali ortadan kalktı
             builder.Property(x => x.advert_date).IsRequired();
             builder.Property(x => x.advert_title).IsRequired();
             builder.Property(x => x.advert_text).IsRequired();
