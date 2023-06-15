848b70e7f8d0de95ead1a795e64633856b48d17b
Elif ÇÖL <elif.col@std.yeditepe.edu.tr>
2023-06-15 10:15:34 +0300

    Interfacelerin classları oluşturuldu...

---
+++ Infrastructure/Data/Postgres/Repositories/Interface/ICategoriesRepository.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Data.Postgres.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Postgres.Repositories.Interface
+{
+    public interface ICategoriesRepository
+    {
+    }
+}
