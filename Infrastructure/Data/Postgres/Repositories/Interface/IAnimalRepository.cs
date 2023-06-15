92b16b4c4cc0fb7fe1dbc12dbf077da19f18e148
caglaozy <caglaozyy@gmail.com>
2023-06-15 10:33:38 +0300

    AnimalRepository  IAnimalRepository  created

--- Infrastructure/Data/Postgres/Repositories/Interface/IAnimalRepository.cs
+++ Infrastructure/Data/Postgres/Repositories/Interface/IAnimalRepository.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Infrastructure.Data.Postgres.Entities;
+using Infrastructure.Data.Postgres.Repositories.Base.Interface;
 
-namespace Infrastructure.Data.Postgres.Repositories.Interface
+namespace Infrastructure.Data.Postgres.Repositories.Interface;
+
+public interface IAnimalRepository : IRepository<Animal, int>
 {
-    public interface IAnimalRepository : Repository<IAnimalRepository, string> , IAnimalRepository
-    {
-        public IAnimalRepository(Postg)
-    }
+    Task<Animal> GetByIdAsync(int id);
 }
