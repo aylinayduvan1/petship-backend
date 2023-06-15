dcb8c9c04ad6f236fae982b8c41be234fe696131
caglaozy <caglaozyy@gmail.com>
2023-06-15 10:34:14 +0300

    IAnimalRepository AnimalRepo created

--- Infrastructure/Data/Postgres/Repositories/AnimalRepository.cs
+++ Infrastructure/Data/Postgres/Repositories/AnimalRepository.cs
@@ -1,12 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Infrastructure.Data.Postgres.Entities;
+using Infrastructure.Data.Postgres.EntityFramework;
+using Infrastructure.Data.Postgres.Repositories.Base;
+using Infrastructure.Data.Postgres.Repositories.Interface;
+using System.Linq.Expressions;
 
-namespace Infrastructure.Data.Postgres.Repositories
+namespace Infrastructure.Data.Postgres.Repositories;
+
+public class AnimalRepository : Repository<User, int>, IAnimalRepository
 {
-    internal class AnimalRepository
+    public AnimalRepository(PostgresContext postgresContext) : base(postgresContext)
     {
+
+    }
+
+    public Task AddAsync(Animal entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddRangeAsync(IEnumerable<Animal> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Animal?> FirstOrDefaultAsync(Expression<Func<Animal, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<Animal>> GetAllAsync(Expression<Func<Animal, bool>>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Animal?> GetAsync(Expression<Func<Animal, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> GetCountAsync(Expression<Func<Animal, bool>>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Animal entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRange(IEnumerable<Animal> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UntrackEntity(Animal entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Animal entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateRange(IEnumerable<Animal> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<Animal> IAnimalRepository.GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
     }
 }
