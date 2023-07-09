6f88e218f8ac3470e52268c393f9c8b85938f0ce
ElifCol <hhy.elif.115@gmail.com>
2023-07-09 14:16:16 +0300

    whole services that include information is created...

---
+++ Business/Services/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Business.Models.Response;
+using Business.Services.Base;
+using Business.Services.Interface;
+using Business.Utilities.Mapping.Interface;
+using Infrastructure.Data.Postgres;
+using Infrastructure.Data.Postgres.Entities;
+using Infrastructure.Data.Postgres.Repositories.Base.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class CategoryService : BaseService<Categories, int, CategoryInfoDTO>, ICategoryService
+    {
+        public CategoryService(IUnitOfWork unitOfWork, IRepository<Categories, int> repository, IMapperHelper mapperHelper) : base(unitOfWork, repository, mapperHelper)
+        {
+        }
+    }
+}
