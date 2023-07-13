be48bc0156af933db46f3149cfe0e9a1b58c3425
aylinayduvan <aylinnayduvann@gmail.com>
2023-07-13 09:19:04 +0300

    status gitti

--- Business/Models/Response/UserProfileDto.cs
+++ Business/Models/Response/UserProfileDto.cs
@@ -6,7 +6,16 @@ public class UserProfileDto
 {
     public int Id { get; set; } = default!;
     public string UserName { get; set; } = default!;
-    public string FullName { get; set; } = default!;
+    public string user_surname { get; set; } = default!;
+    public string user_sex { get; set; } = default!;
+
+    public string user_bdate { get; set; } = default!;
+
+    public bool animal_history { get; set; } = default!;
+
+    public bool animal_exist { get; set; } = default!;
+
     public string Email { get; set; } = default!;
     public UserType UserType { get; set; }
 }
+
